0b3ac52f5d33d57c2e3ff21f0ceedfb8358122cf
Ronald <rontbrilhante@Gmail.com>
2022-04-23 22:27:29 -0400

    Had to redo git local , this is a new commit and final to put on github.

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+//! Pipeviewer library documentation.
+
+pub mod args;
+pub mod read;
+pub mod stats;
+pub mod write;
+
+const CHUNK_SIZE: usize = 16 * 1024;
---
+++ src/main.rs
@@ -0,0 +1,36 @@
+///
+///
+use crossbeam::channel::{bounded, unbounded};
+use pipeviewer::{args::Args, read, stats, write};
+use std::io::Result;
+use std::thread;
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    // destructuring to pass to each  thread, begin section
+    let Args {
+        infile,
+        outfile,
+        silent,
+    } = args;
+
+    let (stats_tx, stats_rx) = unbounded();
+    let (write_tx, write_rx) = bounded(1024);
+
+    let read_handle = thread::spawn(move || read::read_loop(&infile, stats_tx, write_tx));
+    let stats_handle = thread::spawn(move || stats::stats_loop(silent, stats_rx));
+    let write_handle = thread::spawn(move || write::write_loop(&outfile, write_rx));
+
+    // crash if any threads have crashed
+    // .join returns a thread::Result<io::Result<()>>
+    let read_io_result = read_handle.join().unwrap();
+    let stats_io_result = stats_handle.join().unwrap();
+    let write_io_result = write_handle.join().unwrap();
+
+    //Read an error if any thread has returned an error
+    read_io_result?;
+    stats_io_result?;
+    write_io_result?;
+
+    Ok(())
+}
